4db1a64e179b1b825e4398add81cf7270e460a79
ajbt200128 <austin@cutedogs.org>
2024-07-04 18:05:49 -0700

    fix string test

--- core/tests/nix/basic/eval-okay-string.nix
+++ core/tests/nix/basic/eval-okay-string.nix
@@ -1,5 +1,7 @@
+# Note this test no longer passes in nix, and has been modified to pass in nickel
+# This is because nickel doesn't do any path normalization, and nix does
 [
-  (("foobar/a/b/c/d/foo/xyzzy/foo.txt/../foo/x/yescape: \"quote\" \n \\end\nof\nlinefoobarblaatfoo$bar$\"$\"$") == ("foo" + "bar"
+  (("foobar/a/b/c/d/foo/bar/../xyzzy/./foo.txt/../foo/x/yescape: \"quote\" \n \\end\nof\nlinefoobarblaatfoo$bar$\"$\"$") == ("foo" + "bar"
     + toString (/a/b + /c/d)
     + toString (/foo/bar + "/../xyzzy/." + "/foo.txt")
     + ("/../foo" + toString /x/y)
