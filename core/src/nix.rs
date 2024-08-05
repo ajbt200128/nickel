65d4a09e57f06ef31f6e968b2c5088d8f82612cb
ajbt200128 <austin@cutedogs.org>
2024-08-04 21:53:58 -0700

    more comment updates

--- core/src/nix.rs
+++ core/src/nix.rs
@@ -406,7 +406,6 @@ impl ToNickel for rnix::ast::Expr {
                     // the param is a pattern as we generaly see in NixOS modules (`{pkgs, lib,
                     // ...}:`
                     rnix::ast::Param::Pattern(pat) => {
-                        // TODO: Does not support if args are empty, e.g. nix`{}: 1`
                         use crate::term::pattern::*;
                         // Pattern alias i.e. args@{x,y,z}:
                         let alias = match pat.pat_bind() {
